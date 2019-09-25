1bf647930dc991538bdab44df34657a709dc3365
Zenwinks <salle.lucas@hotmail.fr>
2019-09-25 17:48:05 +0200

    from java to kotlin

---
+++ app/src/main/java/epsi/b3/team3_kotlin_project/MainActivity.kt
@@ -0,0 +1,12 @@
+package epsi.b3.team3_kotlin_project
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/test/java/epsi/b3/team3_kotlin_project/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package epsi.b3.team3_kotlin_project
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, (2 + 2).toLong())
+    }
+}
